f59e00738b12b5b696249342d8423bfa2b72d323
mukesh <109419653+mukeshsuvendheran@users.noreply.github.com>
2026-04-28 11:20:36 +0530

    Delete experiment directory

--- experiment/Button/button.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as GPIO
-import time
-
-GPIO.setmode(GPIO.BCM)  
-BUTTON_PIN = 18
-while True:
-       if (GPIO.input(BUTTON_PIN) == GPIO.HIGH):
-           print("Button Pressed")
-           time.sleep(0.5)  
